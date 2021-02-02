7649ab12e2eac46829e3d98046cfe73b07c9309b
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-02-02 13:17:48 -0800

    scroll snap testing

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<main>
+	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, et asperiores. Rerum recusandae aut dolore, ex illum, distinctio, quis quia voluptatum veniam reprehenderit dolorum autem tempore veritatis nostrum mollitia optio!</p>
+	<p>Quo commodi odio voluptatibus. Accusantium amet quod, non porro aliquam delectus autem iste atque laboriosam, nesciunt quisquam iusto at ullam explicabo veniam optio neque necessitatibus sapiente. Commodi nobis maiores tempora?</p>
+	<p>Facere repellendus assumenda nam suscipit recusandae sequi dolore tempore perferendis veniam nostrum! Sequi est sit, cumque ipsam, accusamus et optio facilis nobis deserunt doloremque architecto maxime aut, unde quasi harum.</p>
+	<p>Aliquam, voluptates. Assumenda, atque neque quaerat eligendi magnam numquam recusandae minus cupiditate omnis! Recusandae nulla, quos nemo nobis quibusdam totam ipsa nostrum mollitia voluptas blanditiis, vitae et magnam voluptatibus incidunt?</p>
+	<p>Odio hic pariatur sequi adipisci. Mollitia dicta eum eligendi officia, eveniet ea nobis, laborum sit, eaque sunt nostrum saepe asperiores itaque quibusdam. Impedit fuga velit odit. Nemo commodi recusandae harum?</p>
+	<p>Est maxime illo vero corporis eligendi tenetur, temporibus quae sit consequatur mollitia omnis accusamus vel nisi ab quibusdam ut, dolor pariatur architecto dolorem suscipit ullam corrupti molestiae, veniam sequi! Possimus.</p>
+	<p>Quia optio quod distinctio minus ut eligendi repellendus totam, magni et perspiciatis consequatur doloribus sunt voluptates a culpa ipsa, dolorum beatae ex exercitationem. Rerum veritatis repellendus dolore cum. Culpa, aspernatur.</p>
+	<p>Aspernatur, magnam eum. Repellendus officiis ipsa vitae dolores nam necessitatibus et dignissimos sunt totam, quod ab dolore consequuntur inventore voluptate consequatur atque cum eius numquam voluptas. Quas veritatis alias debitis!</p>
+	<p>At error nesciunt impedit magnam soluta sequi dolore quae cumque dolor officiis enim maiores et dignissimos veniam laborum est itaque, exercitationem delectus debitis non amet? Dolores laudantium pariatur provident veniam?</p>
+	<p>Veritatis esse aliquid eligendi voluptas nemo explicabo magni? Exercitationem, quo. Cupiditate placeat voluptate aut quidem at rem iste quasi similique, nisi, deserunt possimus quod sequi nobis voluptatibus, obcaecati in dicta?</p>
+</main>
+
+<style lang="scss">
+	main {
+		scroll-snap-type: y mandatory;
+		height: 100vh;
+		overflow: scroll;
+	}
+	
+	p {
+		background-color: #2b2b2b;
+		color: white;
+		height: 100vh;
+		scroll-snap-align: center;
+	}
+
+</style>
